5c84443441eebe88604b415282e1135809b81dd5
jsierra93 <jsierra93@hotmail.com>
2021-11-11 10:07:12 -0500

    Se elimina target

--- target/test-classes/karate-config.js
+++
@@ -1,9 +0,0 @@
-function fn(){
-  karate.configure('connectTimeout', 500000);
-  karate.configure('readTimeout', 500000);
- config = {
-    urlBase : 'https://api-payment.getsandbox.com:443'
-    }
-karate.log('get variable from karate-config => ', config)
-return config;
-}
